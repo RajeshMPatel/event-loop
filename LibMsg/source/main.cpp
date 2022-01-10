85387daba2102927c8427bfd74f6586cbb3900ff
Rajesh Patel <RajeshMPatel@gmail.com>
2022-01-10 13:48:45 -0800

    Added comments

--- LibMsg/source/main.cpp
+++ LibMsg/source/main.cpp
@@ -13,6 +13,9 @@ TestMsgThread g_rMessageThread;
 int main(int argc, const char * argv[]) {
   std::cout << "Welcome to the world of Asynchronous Programming!!" << std::endl;
   
+  // We start our message thread. This is separate from the main application thread.
+  // All our processing is done in this thread
+  
   g_rMessageThread.Start();
   g_rMessageThread.PostMessage(cInitialize, 0, 0);
 
